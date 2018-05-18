59b12c34a1a1d6212371b79a1c58b11dfd115ef4
CerenEkmekciler <ekmekcilercerenn@gmail.com>
2018-05-18 14:47:47 +0300

    Create kullanici.php

---
+++ kullanici.php
@@ -0,0 +1,168 @@
+<fieldset height="1000" >
+<legend color="red" border="5"><h3>Kullanıcı Sayfanız</h3></legend>
+<input type="button" name="button" id="buton" value="Çıkış Yap" onclick="window.location ='http://localhost:90/logout.php'"></input>
+<table align="center" border="5" cellspacing="10" width="700" height="300" cellspacing="10">
+      <tr>
+         <td bgcolor="#F8E0E0" colspan="2">Eğitim Programı</td>
+      </tr>
+      <tr>
+         <td rowspan="2" bgcolor="#F78181">Çocuklar İçin Afet Bilinci Eğitimi</td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 21/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 29/06/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="#F78181" rowspan="2">Afet Risk Azaltma Politikaları </td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 27/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 30/07/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="#F78181" rowspan="2">Temel İlk Yardım Bilgisi </td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 30/04/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 15/06/2018</td>
+      </tr>
+	  <tr>
+         <td rowspan="2" bgcolor="#FE2E2E">Özel Gereksinimli Bireyler İçin Acil Durum Ve Afet Yönetimi </td>
+         <td bgcolor="#FE2E2E">Başlangıç Tarihi:
+		 20/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#FE2E2E">Bitiş Tarihi:
+		 02/07/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="red" rowspan="2">Acil Durum Bilgisi Ve Yönetimine Giriş </td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 05/03/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 04/05/2018</td>
+      </tr>
+	  <tr>
+         <td rowspan="2" bgcolor="red">Afet Senaryosu Ve Tatbikatlar </td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 23/04/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 14/05/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="red" rowspan="2">Oyunlaştırılmış Afet Eğitimleri</td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 08/01/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 06/04/2018</td>
+      </tr>
+</table>
+
+<input type="button" name="button" id="devamsizlik" value="Ders Devam Kontrol" onclick="window.location ='http://localhost:90/devamsizlik.php'">
+ <input type="button" name="button" id="derslerim" value="Aldığınız Dersler" onclick="window.location ='http://localhost:90/derslerim.php'">
+ <input type="button" name="button" id="ucret" value="Kurs Ücretleri" onclick="window.location ='http://localhost:90/ucret.php'">
+<input type="button" name="button" id="cikis" value="Kurs Değerlendirme" onclick="window.location ='http://localhost:90/anket.php'"></input>
+
+<style type="text/css">
+ #devamsizlik{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:200px;
+	font-family:Arial;
+}
+
+#derslerim{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:200px;
+	margin:50px;
+	font-family:Arial;
+	}
+	
+#ucret{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:10px;
+	font-family:Arial;
+}
+
+#cikis{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:70px;
+	font-family:Arial;
+	}
+	
+#buton{
+	
+
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin-left:1100px;
+	font-family:Arial;
+	}
+}
+</style>
+</fieldset>
