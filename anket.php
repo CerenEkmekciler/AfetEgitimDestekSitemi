55a8c105f0fa1d127cb99f3ba9872271753cdf23
CerenEkmekciler <ekmekcilercerenn@gmail.com>
2018-05-18 14:58:10 +0300

    Create anket.php

---
+++ anket.php
@@ -0,0 +1,17 @@
+<form action="anketoyla.php" method="post">
+<tr>
+
+
+      <td colspan="2" align="left">&nbsp;&nbsp;&nbsp;&nbsp;Aldığınız Ders Eğitime Verdiğiniz Ücreti Karşılıyor mu?</td>
+
+
+    </tr><br>
+	<br>
+	
+	<input type="radio" name="ders" value="evet"/>Evet<br/>
+
+	<input type="radio" name="ders" value="hayır"/>Hayır<br/>
+	<br>
+
+<input type="submit" name="submit" value="Cevabımı ilet"></input>
+</form>
