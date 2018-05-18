287355bb94dfca7d372b7cc8c2917b1c1f6d0ed6
CerenEkmekciler <ekmekcilercerenn@gmail.com>
2018-05-18 14:49:51 +0300

    Create hoca.php

---
+++ hoca.php
@@ -0,0 +1,107 @@
+<table align="center" border="5" width="700" height="300" cellspacing="10">
+      <tr>
+         <td bgcolor="#F8E0E0" colspan="2">Eğitim Programı</td>
+      </tr>
+      <tr>
+         <td rowspan="2" bgcolor="#F78181">Çocuklar İçin Afet Bilinci Eğitimi</td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 21/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 29/06/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="#F78181" rowspan="2">Afet Risk Azaltma Politikaları </td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 27/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 30/07/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="#F78181" rowspan="2">Temel İlk Yardım Bilgisi </td>
+         <td bgcolor="#F78181">Başlangıç Tarihi:
+		 30/04/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#F78181">Bitiş Tarihi:
+		 15/06/2018</td>
+      </tr>
+	  <tr>
+         <td rowspan="2" bgcolor="#FE2E2E">Özel Gereksinimli Bireyler İçin Acil Durum Ve Afet Yönetimi </td>
+         <td bgcolor="#FE2E2E">Başlangıç Tarihi:
+		 20/05/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="#FE2E2E">Bitiş Tarihi:
+		 02/07/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="red" rowspan="2">Acil Durum Bilgisi Ve Yönetimine Giriş </td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 05/03/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 04/05/2018</td>
+      </tr>
+	  <tr>
+         <td rowspan="2" bgcolor="red">Afet Senaryosu Ve Tatbikatlar </td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 23/04/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 14/05/2018</td>
+      </tr>
+	  <tr>
+         <td bgcolor="red" rowspan="2">Oyunlaştırılmış Afet Eğitimleri</td>
+         <td bgcolor="red">Başlangıç Tarihi:
+		 08/01/2018</td>
+      </tr>
+      <tr>
+         <td bgcolor="red">Bitiş Tarihi:
+		 06/04/2018</td>
+      </tr>
+</table>
+   
+ 
+<input type="button" name="button" id="kayitlar" value="Dersime Kayıtlı Öğrencileri Görüntüle" onclick="window.location ='http://localhost:90/orhanduru.php'">
+<input type="button" name="button" id="cikis" value="Çıkış Yap" onclick="window.location ='http://localhost:90/index'"></input>
+ <style type="text/css">
+
+
+#kayitlar{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	margin:20px;
+	margin-left:450px;
+	font-family:Arial;
+}
+
+#cikis{
+	background-color:#ffffff;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	color:#f24537;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:10px 24px;
+	font-family:Arial;
+}
+</style>
